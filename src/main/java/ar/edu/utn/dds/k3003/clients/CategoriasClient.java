1e1d78c9f434f1aa060db4c3537ee74e900ba356
lseijasferraudo <lseijasferraudo@frba.utn.edu.ar>
2026-06-12 22:35:51 -0300

    afs

--- src/main/java/ar/edu/utn/dds/k3003/clients/CategoriasClient.java
+++ src/main/java/ar/edu/utn/dds/k3003/clients/CategoriasClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name = "categorias_client",
+        name = "categoriasclient",
         url = "${donaciones.url}"
 )
 public interface CategoriasClient {
